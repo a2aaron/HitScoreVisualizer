283ad6941472d54a3c248276f3493fc0b6fe1af5
Metalit <48568911+Metalit@users.noreply.github.com>
2025-02-16 23:22:54 -0700

    Remove unnecessary types

--- include/json/Config.hpp
+++ include/json/Config.hpp
@@ -6,19 +6,6 @@
 #include "config-utils/shared/config-utils.hpp"
 
 namespace HSV {
-    // why cordl why
-    struct EquatableColor : ConfigUtils::Color {
-        bool operator==(EquatableColor const& rhs) const { return r == rhs.r && g == rhs.g && b == rhs.b && a == rhs.a; };
-        EquatableColor(UnityEngine::Color const& rhs) : ConfigUtils::Color(rhs) {}
-        EquatableColor() = default;
-    };
-
-    struct EquatableVec3 : ConfigUtils::Vector3 {
-        bool operator==(EquatableVec3 const& rhs) const { return x == rhs.x && y == rhs.y && z == rhs.z; };
-        EquatableVec3(UnityEngine::Vector3 const& rhs) : ConfigUtils::Vector3(rhs) {}
-        EquatableVec3() = default;
-    };
-
     DECLARE_JSON_STRUCT(Judgement) {
        private:
         NAMED_VALUE(std::string, UnprocessedText, "text");
@@ -33,7 +20,7 @@ namespace HSV {
         NAMED_VALUE_DEFAULT(int, Threshold, 0, "threshold");
         NAMED_VALUE_OPTIONAL(bool, Fade, "fade");
         TokenizedText Text;
-        EquatableColor Color;
+        ConfigUtils::Color Color;
 
         Judgement(int threshold, std::string text, UnityEngine::Color color, bool fade = false) :
             Threshold(threshold),
@@ -81,21 +68,21 @@ namespace HSV {
         NAMED_VALUE_OPTIONAL(float, FixedPosY, "fixedPosY");
         NAMED_VALUE_OPTIONAL(float, FixedPosZ, "fixedPosZ");
         NAMED_VALUE_OPTIONAL(bool, UseFixedPos, "useFixedPos");
-        NAMED_VALUE_OPTIONAL(EquatableVec3, UnprocessedFixedPos, "fixedPosition");
-        NAMED_VALUE_OPTIONAL(EquatableVec3, UnprocessedPosOffset, "targetPositionOffset");
+        NAMED_VALUE_OPTIONAL(ConfigUtils::Vector3, UnprocessedFixedPos, "fixedPosition");
+        NAMED_VALUE_OPTIONAL(ConfigUtils::Vector3, UnprocessedPosOffset, "targetPositionOffset");
         DESERIALIZE_FUNCTION(ConvertPositions) {
             if (UseFixedPos.has_value() && UseFixedPos.value())
-                FixedPos = UnityEngine::Vector3(FixedPosX.value_or(0), FixedPosY.value_or(0), FixedPosZ.value_or(0));
+                FixedPos = {FixedPosX.value_or(0), FixedPosY.value_or(0), FixedPosZ.value_or(0)};
             else if (UnprocessedFixedPos.has_value())
-                FixedPos = UnityEngine::Vector3(UnprocessedFixedPos->x, UnprocessedFixedPos->y, UnprocessedFixedPos->z);
+                FixedPos = {UnprocessedFixedPos->x, UnprocessedFixedPos->y, UnprocessedFixedPos->z};
             if (UnprocessedPosOffset)
-                PosOffset = UnityEngine::Vector3(UnprocessedPosOffset->x, UnprocessedPosOffset->y, UnprocessedPosOffset->z);
+                PosOffset = {UnprocessedPosOffset->x, UnprocessedPosOffset->y, UnprocessedPosOffset->z};
         };
         NAMED_VALUE_DEFAULT(int, TimeDependenceDecimalPrecision, 1, "timeDependencyDecimalPrecision");
         NAMED_VALUE_DEFAULT(int, TimeDependenceDecimalOffset, 2, "timeDependencyDecimalOffset");
 
-        std::optional<EquatableVec3> FixedPos;
-        std::optional<EquatableVec3> PosOffset;
+        std::optional<ConfigUtils::Vector3> FixedPos;
+        std::optional<ConfigUtils::Vector3> PosOffset;
         bool HasChainHead;
         bool HasChainLink;
     };
